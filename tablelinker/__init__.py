d051f2201c2693c574145f39ba790f2af5332dcf
Takeshi Sagara <sagara@info-proto.com>
2024-04-03 05:21:03 +0000

    Fix repository URL.

--- tablelinker/__init__.py
+++ tablelinker/__init__.py
@@ -3,7 +3,7 @@ from logging import getLogger
 from .core.table import Table
 from .core.task import Task
 
-__version__ = "1.0.0"  # pyproject.toml と git tag も変更すること
+__version__ = "1.0.0.post2"  # pyproject.toml と git tag も変更すること
 
 logger = getLogger(__name__)
 
